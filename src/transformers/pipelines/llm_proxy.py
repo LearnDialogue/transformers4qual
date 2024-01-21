88223e14ca3a4b00df28a2d2431ef05948a03d38
Amogh Mannekote <msamogh@gmail.com>
2024-01-21 16:29:06 -0500

    Add timeout

--- src/transformers/pipelines/llm_proxy.py
+++ src/transformers/pipelines/llm_proxy.py
@@ -3,6 +3,12 @@ import asyncio
 from langchain_core.messages.ai import AIMessage
 from langchain.callbacks import get_openai_callback
 
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)
+
 from ..configuration_utils import PretrainedConfig
 from ..modeling_utils import PreTrainedModel
 from .base import Pipeline
@@ -19,11 +25,16 @@ class LangchainModelForProxyLLM(PreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
         self.runnable = config.runnable
-    
+
+    @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
+    async def _forward_single(self, model_input):
+        return await self.runnable.ainvoke(model_input)
+                
     async def _forward_concurrent(self, model_input):
+        # Call _forward_single in a loop
         return await asyncio.gather(
             *[
-                self.runnable.ainvoke({k: v[i] for k, v in model_input.items()})
+                self._forward_single({k: v[i] for k, v in model_input.items()})
                 for i in range(len(model_input[list(model_input.keys())[0]]))
             ]
         )
