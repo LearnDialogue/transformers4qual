6d2c4aa762294ec8b9b1fb1d97a8a296620416a5
Amogh Mannekote <msamogh@gmail.com>
2024-01-20 19:18:18 -0500

    Make Runnable async

--- src/transformers/pipelines/llm_proxy.py
+++ src/transformers/pipelines/llm_proxy.py
@@ -1,4 +1,7 @@
+import asyncio
+
 from langchain_core.messages.ai import AIMessage
+from langchain.callbacks import get_openai_callback
 
 from ..configuration_utils import PretrainedConfig
 from ..modeling_utils import PreTrainedModel
@@ -16,6 +19,14 @@ class LangchainModelForProxyLLM(PreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
         self.runnable = config.runnable
+    
+    async def _forward_concurrent(self, model_input):
+        return await asyncio.gather(
+            *[
+                self.runnable.ainvoke({k: v[i] for k, v in model_input.items()})
+                for i in range(len(model_input[list(model_input.keys())[0]]))
+            ]
+        )
 
     def forward(self, model_input):
         if self.config.mock_llm_call:
@@ -23,10 +34,7 @@ class LangchainModelForProxyLLM(PreTrainedModel):
                 f"The score is {random.randint(-1, 8)}. This was generated from a mock call."
                 for i in range(len(model_input[list(model_input.keys())[0]]))
             ]
-        return [
-            self.runnable.invoke({k: v[i] for k, v in model_input.items()})
-            for i in range(len(model_input[list(model_input.keys())[0]]))
-        ]
+        return asyncio.run(self._forward_concurrent(model_input))
 
 
 class LLMProxyPipeline(Pipeline):
@@ -37,7 +45,9 @@ class LLMProxyPipeline(Pipeline):
         return {"model_input": inputs}
 
     def _forward(self, model_inputs, **kwargs):
+        print(f"Executing Langchain runnable.")
         output = self.model(**model_inputs)
+        print(f"Finished executing Langchain runnable.")
         return output
 
     def postprocess(self, output, **kwargs):
